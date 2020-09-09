36ae702e9f5ac598dde9cbf473b230e16bc0c968
riverwide3 <dlmossmus@gmail.com>
2020-09-09 15:46:01 +0200

    added test case C

--- CW_Kata_Find_Odd_Int_A.js
+++ CW_Kata_Find_Odd_Int_A.js
@@ -9,19 +9,23 @@ function findOdd(A) {
                 n++;
             }
         }
+        console.log(n)
         if (n % 2 !== 0) {
             return A[i];
         }
     }
     return 0;
+    
 }
 
 // Testing
 try {
     let A = [20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5];
     let B = [4, 2, 6, 4, 6, 2, 6];
+    let C = [2, 5, 1, 2, 5, 1];
     assert.strictEqual(findOdd(A), 5);
     assert.strictEqual(findOdd(B), 6);
+    assert.strictEqual(findOdd(C), 0);
     
     
     console.log('Passed');
