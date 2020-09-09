f0dc2077cc4ab9d12ab703a8143b4e950c79e460
riverwide3 <dlmossmus@gmail.com>
2020-09-09 14:10:34 +0200

    CW_Kata_Unique_In_Order_Current

---
+++ CW_Kata_Unique_In_Order.js
@@ -0,0 +1,24 @@
+const { assert } = require('assert');
+
+var uniqueInOrder = function(iterable){
+    //your code here - remember iterable can be a string or an array
+    let resultArray = [];
+
+    for (let i = 0; i < iterable_1.length; i++) {
+        if (iterable_1[i] !== iterable_1[i + 1]) {
+            resultArray.push(iterable_1[i]);      }
+    }
+    return resultArray;
+}
+
+
+//testing
+
+try {
+    let iterable_1 = 'AAAABBBCCDAABBB';
+    assert.strictEqual(uniqueInOrder(iterable_1), ['A', 'B', 'C', 'D', 'A', 'B']);    
+    
+    console.log('Passed')
+} catch (err) {
+    console.log(err.message);
+}
